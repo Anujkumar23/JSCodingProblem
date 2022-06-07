24431d0c2b35a8a1851e63314905ac829b3cc78c
anuj <anujthakur315@gmail.com>
2022-06-07 22:43:47 +0530

    add comment

--- duplicateNumber.js
+++ duplicateNumber.js
@@ -1,9 +1,9 @@
 //  Program to find the duplicate number from an array
-// We first create a function name duplicateNumber which contaion a parameter
-// We create an empty array to store the duplicate no.
-// After we itrate a for loop over num parameter
-// Now first we need to compare element at index i with all other element o .For that we need to create an other for loop it would also start from j=0 now if we compare i=j it will give us a duplicate value but 
-
+// 1.We first create a function name duplicateNumber which contaion a parameter
+//2. We create an empty array to store the duplicate no.
+//3. After we itrate a for loop over num parameter
+//4. Now first we need to compare element at index i with all other element present in array .For that we need to create an other for loop it would also start from j=0 now we will apply if condition on index i and j that if i!==j and element of i is strictly equal to element of j and our result array does not include that element already that push the element in our result array.
+// after that we will return the result array and call the function by passsing the argument.
 let arr = [1, 2, 3, 3, 4,5,5,6,7,7];
 
 function duplicateNumber(num) {
