65f4ecf1c450817120888085a9cbfebf2e88dbc6
anuj <anujthakur315@gmail.com>
2022-06-07 22:48:21 +0530

    remove extra spaces

--- sumOfNumber.js
+++ sumOfNumber.js
@@ -1,6 +1,6 @@
 /*1.In this program first we declare  a variable  which contain  some element
   2.Then we declare a function name sumOfNumber which takes a parameter also
-  3.Then we declare a  variable sum to store the result. at staring the value is 0
+  3.Then we declare a  variable sum to store the result. At starting the value is 0
   4.Now we use for loop to itrate over each element of arr and add current index value to the sum variable
   5.Then return sum call the function and pass argument iside the funtion and print the result   */
        let  arr= [1,2,3,4,5]
